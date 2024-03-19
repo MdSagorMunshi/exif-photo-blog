0037e6d619307da17c9e7659e5357273f9664449
Md Sagor Munshi <121169140+MdSagorMunshi@users.noreply.github.com>
2024-03-19 09:50:06 +0600

    Update RepoLink.tsx

--- src/components/RepoLink.tsx
+++ src/components/RepoLink.tsx
@@ -9,7 +9,7 @@ export default function RepoLink() {
         Made with
       </span>
       <Link
-        href="http://github.com/sambecker/exif-photo-blog"
+        href="https://github.com/MdSagorMunshi/exif-photo-blog"
         target="_blank"
         className={clsx(
           'flex items-center gap-0.5',
